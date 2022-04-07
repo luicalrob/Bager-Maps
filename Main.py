435f8472d5ffd891975bb1e53437cf0e958fc1fe
luicalrob <luiscalderonr00@gmail.com>
2022-04-07 12:12:03 +0200

    lectura de datos

--- Main.py
+++ Main.py
@@ -1,2 +1,9 @@
-#Programa de python
-print("documento inicial");
+"""
+@author Luis Calderon
+
+"""
+
+import pandas as pd
+datos = pd.read_csv('Sample test file - Sheet1.csv',header = 0)
+print(datos)
+print(datos['First Name'])
